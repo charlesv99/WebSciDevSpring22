d1a51c7e484387c13a6a5d1ba2983d6f59da7b69
charlesv99 <villac2@rpi.edu>
2022-01-21 10:11:18 -0500

    finished up js and css now to play with design

--- lab1/app.js
+++ lab1/app.js
@@ -1,15 +1,27 @@
 function load(x){
     var i=0;
     function loop(){
-        setTimeout(()=>{
-            if(i<x.length){
-                $("#vwrap").prepend(
-                    x[i++]
-                 );
-            loop();    
-            }
-        },2000);
+        if(i<5){
+            setTimeout(()=>{
+                if(i<x.length){
+                    $("#vwrap").prepend(
+                        x[i++]
+                     );
+                loop();    
+                }
+            },0);
+        }else{
+            setTimeout(()=>{
+                if(i<x.length){
+                    $("#vwrap").prepend(
+                        x[i++]
+                     );
+                loop();    
+                }
+            },3000);
+        }
     };
+    
     loop();
 };
 $(function(){ 
@@ -22,11 +34,26 @@ $(function(){
         for(let i=0;i<data.articles.length;i++){
           var mydata=data.articles[i];
           var author=mydata.author;
+          if(author==null){
+              author=mydata.source.name;
+          }else{
+              author+=" - "+mydata.source.name;
+          }
+          var title=mydata.title;
           var desc=mydata.description;
-          var words=`<div>${desc}</div>`;
+          var imageurl=mydata.urlToImage;
+          var url=mydata.url;
+          var words=`<a href="${url}"><div class="articles id="${i}"onclick="${url}">
+            <img src="${imageurl}"/>
+            <div class="txtCont">
+                <div class="title">${title}</div>
+                <div class="author">${author}</div>
+                <div class="desc">${desc}</div>
+            </div>
+          </div></a>`;
           art.push(words)
 
         }
-    load(data);
+    load(art);
     });
    });
