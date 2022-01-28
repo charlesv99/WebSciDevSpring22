f11f87c8f198dd305de5ae954352ca6b78c5b8a2
charlesv99 <villac2@rpi.edu>
2022-01-28 11:12:25 -0500

    some minimal css

--- lab2/app.js
+++ lab2/app.js
@@ -7,17 +7,6 @@ const main = document.querySelector("#main");
 const discription = document.querySelector("#discription");
 const image = document.querySelector("#image");
 
-var element = document.getElementById('cLoc');
-element.addEventListener('onclick',setLoc() ,false);
-
-wUpdate('Miami');
-
-input.onsubmit = (x) => {
-        x.preventDefault();
-        wUpdate(city.value);
-        city.value = "";
-   
-};
 
 function setLoc() {
     var pos = navigator.geolocation;
@@ -29,14 +18,14 @@ function setLoc() {
 };
 
 function success(data) {
+
     var lat = data.coords.latitude;
     var long = data.coords.longitude;
     wUpdateLL(lat,long);
-    lat="";
-    long="";
     
 };
-wUpdateLL = (lat,long) => {
+
+function wUpdateLL (lat,long) {
     const req = new XMLHttpRequest();
     req.open(
         "GET",
@@ -48,15 +37,15 @@ wUpdateLL = (lat,long) => {
           } else {
             var data = JSON.parse(req.response);
             cityName.innerHTML = data.name;
-            Temp.innerHTML = `${Math.round(data.main.temp- 273.15)}°C`;
+            Temp.innerHTML = `${Math.round(data.main.temp- 273.15)} Celsius`;
             main.innerHTML = data.weather[0].main;
             discription.innerHTML = data.weather[0].description;
             image.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
           }
     };
 };
-
-wUpdate = (city) => {
+setLoc();
+function wUpdate (city) {
     const req = new XMLHttpRequest();
     req.open(
         "GET",
