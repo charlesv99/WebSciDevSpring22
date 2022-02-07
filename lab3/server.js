3dc57cd9e57e9978f8478300141cd95e493c0a38
charlesv99 <villac2@rpi.edu>
2022-02-07 16:07:30 -0500

    quote works now onto the color schemes

--- lab3/server.js
+++ lab3/server.js
@@ -2,39 +2,36 @@ const express = require('express');
 const app = express();
 const axios = require('axios');
 
+
+app.use(express.static('public'));
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/generate', async (req, res) => {
-    let quote = '';
-    let author = '';
-    let hex='';
+app.get('/generate', (req, res) => {
     console.log("/generate endpint called");
     
     axios.get('https://api.fisenko.net/v1/quotes/en/random')
     .then(function (response) {
-        quote = response.data.text;
-        author = response.data.author.name;
-    })
-    .catch(function (error) {
-        console.log(error);
-    });
-    console.log(quote);
-    axios.get('http://palett.es/API/v1/palette')
-    .then(function (response) {
-        hex = response.data;
-        console.log(hex);
-        console.log(hex[0]);
+        res.send(response.data);
+
     })
     .catch(function (error) {
         console.log(error);
     });
     
-
-
 });
 
+app.get('/scheme', (req, res) => {
+    axios.get('http://palett.es/API/v1/palette')
+        .then(function (response) {
+            console.log(response.data);
+            res.send(response.data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+})
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
 	console.log('Listening on *:'+port)
