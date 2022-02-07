48e00dd479bdcd9fe2eaa380e4f48bb06287e786
charlesv99 <villac2@rpi.edu>
2022-02-07 13:01:35 -0500

    finished api calls

--- lab3/README.txt
+++ lab3/README.txt
@@ -1,8 +1,10 @@
 So i decided to use a quote api that gives u random quote and their authors and some other data on the text. 
 I plan to incorporate a second api that generates color schemes and change the ui along with the text on every generation.
 https://github.com/fisenkodv/dictum#parameters
-https://colormind.io/api-access/
+https://palett.es/api
 first i set up the server, i used the powerpoints from class to guide me.
-than i researched how to use node-fetch to simplify my life
-https://www.npmjs.com/package/node-fetch
+than i researched how to use axios to simplify my life
+https://axios-http.com/docs/intro
 than outlined my html containers
+the plan is to have a simple good looking welcoming ui 
+that shows u a dice button that when clicked generates a ui with a quote and a generated colorscheme.
--- lab3/server.js
+++ lab3/server.js
@@ -1,14 +1,39 @@
 const express = require('express');
-const app = express()
-const fetch = require('node-fetch');
+const app = express();
+const axios = require('axios');
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
-})
+});
 
-app.get('/generate', (req, res) => {
+app.get('/generate', async (req, res) => {
+    let quote = '';
+    let author = '';
+    let hex='';
     console.log("/generate endpint called");
-})
+    
+    axios.get('https://api.fisenko.net/v1/quotes/en/random')
+    .then(function (response) {
+        quote = response.data.text;
+        author = response.data.author.name;
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    console.log(quote);
+    axios.get('http://palett.es/API/v1/palette')
+    .then(function (response) {
+        hex = response.data;
+        console.log(hex);
+        console.log(hex[0]);
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    
+
+
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
